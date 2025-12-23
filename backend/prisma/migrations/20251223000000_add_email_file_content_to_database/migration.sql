d8949d321b194795ced8c3afa7ec494401aad925
DonnaSko <38009689+DonnaSko@users.noreply.github.com>
2025-12-23 16:06:32 -0500

    Clean up unused database migration files

--- backend/prisma/migrations/20251223000000_add_email_file_content_to_database/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable: Add content column to store file data in database
--- Make path nullable since we'll use content instead
-ALTER TABLE "csl"."EmailFile" 
-ADD COLUMN "content" BYTEA;
-
-ALTER TABLE "csl"."EmailFile" 
-ALTER COLUMN "path" DROP NOT NULL;
-
